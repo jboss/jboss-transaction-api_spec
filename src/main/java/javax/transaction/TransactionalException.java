89ebdfc880292d4f32bb6cf203aae2119c580fe4
Shelly McGowan <smcgowan@redhat.com>
2013-03-14 13:22:19 -0400

    JBEE-132 - add javax.transaction.TransactionalException

---
+++ src/main/java/javax/transaction/TransactionalException.java
@@ -0,0 +1,22 @@
+package javax.transaction;
+
+/**
+ *  The TransactionalException thrown from Transactional annotation interceptors implementation
+ *  contains the original exception as its nested exception and is a RuntimeException; therefore, by
+ *  default any transaction that was started as a result of a Transactional annotation earlier in the call
+ *  stream will be marked for rollback as a result of the TransactionalException being thrown by the Transactional
+ *  interceptor of the second bean.
+ *
+ * * @since:  JTA 1.2
+ *
+ */
+public class TransactionalException extends RuntimeException
+{
+    /**
+     * Create a TransactionalException with a given string and nested Throwable.
+     */
+    public TransactionalException(String s, Throwable throwable)
+    {
+    }
+
+}
